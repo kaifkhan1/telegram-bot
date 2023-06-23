f644cf1e33ce6e86f23b1a28c6173fa83581421d
Kaif Khan <76866688+kaifkhan1@users.noreply.github.com>
2023-06-23 23:55:54 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,35 +10,31 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.post('/newMessage',async(req,res)=>{
     const {message} = req.body
 
-    if(!message){
-        res.end();
-    }
-
-    if(message.text.toLowerCase().includes('start'||'hi'||'hello'||'hey')){
+    if(!message || message.text.toLowerCase().indexOf('salaam')<0){
         await axios.post(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,{
-            chat_id : message.chat.id,
-            text : "Asslamu Alaikum!!, To communicate with me please type Salaam"
-    }).then(res=>{
-        console.log("Message posted");
-        res.end('ok')
-    }).catch(err=>{
-        console.log("Error : ",err);
-        res.end('Error :',err)
-    })
-    }
-    else if(message.text.toLowerCase().includes('salaam')){
-
-    await axios.post(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,{
         chat_id : message.chat.id,
-                text : "Salaam!!"
+        text : "Assalmu Alaikum!!, To communicate with me Say Salaam or salaam"
         }).then(res=>{
             console.log("Message posted");
-            res.end('ok')
         }).catch(err=>{
             console.log("Error : ",err);
             res.end('Error :',err)
         })
-}})
+        return res.end()
+    }
+    
+
+    await axios.post(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage`,{
+        chat_id : message.chat.id,
+        text : "Salaam!!"
+}).then(res=>{
+    console.log("Message posted");
+    res.end('ok')
+}).catch(err=>{
+    console.log("Error : ",err);
+    res.end('Error :',err)
+})
+})
 
 app.listen(3000,()=>{
     console.log("Server is running");
